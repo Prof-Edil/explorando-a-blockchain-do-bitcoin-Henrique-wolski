51977f98baa6232f526a89ca37ab20a51c410d7f
Henrique Wolski <67995384+Henrique-wolski@users.noreply.github.com>
2025-02-02 23:50:31 -0300

    Update 007.sh

--- submission/007.sh
+++ submission/007.sh
@@ -1,18 +1,15 @@
 # Only one single output remains unspent from block 123,321. What address was it sent to?
-block_hash=$(bitcoin-cli getblockhash 123321)
-block_data=$(bitcoin-cli getblock $block_hash 2)
-
-txids=$(echo "$block_data" | jq -r '.tx[].txid')
+blockhash=$(bitcoin-cli getblockhash 123321)
+txids=$(bitcoin-cli getblock "$blockhash" | jq -r '.tx[]')
 
 for txid in $txids; do
-    vout_count=$(bitcoin-cli getrawtransaction $txid 1 | jq '.vout | length')
-
-    for ((i=0; i<vout_count; i++)); do
-        unspent=$(bitcoin-cli gettxout $txid $i)
-        if [ -n "$unspent" ]; then
-            address=$(echo "$unspent" | jq -r '.scriptPubKey.addresses[0]')
-            echo $address
-            exit 0
-        fi
-    done
+  vout_count=$(bitcoin-cli getrawtransaction "$txid" 1 | jq -r '.vout | length')
+  for i in $(seq 0 $((vout_count - 1))); do
+    is_spent=$(bitcoin-cli gettxout "$txid" $i | jq -r '.bestblock' 2>/dev/null) # Check if output is spent
+    if [[ -z "$is_spent" ]]; then # If not spent
+        address=$(bitcoin-cli getrawtransaction "$txid" 1 | jq -r ".vout[$i].scriptPubKey.address")
+        echo "$address"
+        exit 0 # Exit after finding the unspent output
+    fi
+  done
 done

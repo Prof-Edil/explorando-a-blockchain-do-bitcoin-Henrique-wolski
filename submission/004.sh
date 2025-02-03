451de293eab78c98feb5376c8f0e98877ddeea1e
Henrique Wolski <67995384+Henrique-wolski@users.noreply.github.com>
2025-02-02 23:36:58 -0300

    Update 004.sh

--- submission/004.sh
+++ submission/004.sh
@@ -2,10 +2,10 @@
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
 xpub="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
 
-descriptor="tr($xpub/100/*)"
+descriptor="tr($xpub/0/*)"
 
 descriptor_with_checksum=$(bitcoin-cli getdescriptorinfo "$descriptor" | jq -r '.descriptor')
 
-address=$(bitcoin-cli deriveaddresses "$descriptor_with_checksum" "[100]" | jq -r '.[0]')
+address=$(bitcoin-cli deriveaddresses "$descriptor_with_checksum" "[100,100]" | jq -r '.[0]')
 
 echo $address

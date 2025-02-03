524e19f47d17250e3065ca22f83c4670f306271e
Henrique Wolski <67995384+Henrique-wolski@users.noreply.github.com>
2025-02-02 23:51:44 -0300

    Update 008.sh

--- submission/008.sh
+++ submission/008.sh
@@ -1,9 +1,7 @@
 # Which public key signed input 0 in this tx:
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
-tx_data=$(bitcoin-cli getrawtransaction e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163 1)
-
-scriptSig=$(echo "$tx_data" | jq -r '.vin[0].scriptSig.asm')
-
-pubkey=$(echo "$scriptSig" | awk '{print $NF}')
-
-echo $pubkey
+txid="e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163"
+rawtx=$(bitcoin-cli getrawtransaction "$txid" 1)
+pubkey=$(echo "$rawtx" | jq -r '.vin[0].scriptSig.hex' | xxd -r -p | tail -c +2 | head -c -2 | xxd -p)
+decoded_pubkey=$(bitcoin-cli decoderawtransaction "$rawtx" | jq -r '.vin[0].txinwitness[1]')
+echo "$decoded_pubkey"
